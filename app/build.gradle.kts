29f9440506ec39a946139bd6e2be5fffe683b2ce
shabestari <niloufar2012shabestari@gmail.com>
2023-11-01 14:44:26 +0330

    added navigation component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,14 +65,15 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
 
-    //Dagger - Hilt
+
     implementation("com.google.dagger:hilt-android:2.48")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    ksp("com.google.dagger:hilt-android-compiler:2.48")
-    ksp("androidx.hilt:hilt-compiler:1.0.0")
+    // Dagger Hilt
+    implementation ("com.google.dagger:hilt-android:2.48")
     ksp("com.google.dagger:hilt-compiler:2.48")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
@@ -80,6 +81,9 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.3")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+
+    // Compose Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.4")
 
     // Room components
     implementation ("androidx.room:room-runtime:2.5.2")
