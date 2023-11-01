b300310139c1182c9aa8d8ae18e15815420765ec
shabestari <niloufar2012shabestari@gmail.com>
2023-11-01 13:41:46 +0330

    edited package path

--- app/src/main/java/com/androiddev/mytodoapp/data/TodoDao.kt
+++ app/src/main/java/com/androiddev/mytodoapp/data/TodoDao.kt
@@ -1,4 +1,4 @@
-package com.androiddev.mytodoapp.data.models
+package com.androiddev.mytodoapp.data
 
 import androidx.room.Dao
 import androidx.room.Delete
@@ -6,6 +6,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
+import com.androiddev.mytodoapp.data.models.TodoTask
 import kotlinx.coroutines.flow.Flow
 
 @Dao
--- app/src/main/java/com/androiddev/mytodoapp/data/TodoDatabase.kt
+++ app/src/main/java/com/androiddev/mytodoapp/data/TodoDatabase.kt
@@ -2,7 +2,6 @@ package com.androiddev.mytodoapp.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.androiddev.mytodoapp.data.models.TodoDao
 import com.androiddev.mytodoapp.data.models.TodoTask
 
 @Database(entities = [TodoTask::class], version = 1, exportSchema = false)
