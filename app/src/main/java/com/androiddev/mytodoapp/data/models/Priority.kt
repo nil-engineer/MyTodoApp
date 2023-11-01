f4e408505b467e457fe69b967590d9d6050ca6d4
shabestari <niloufar2012shabestari@gmail.com>
2023-11-01 13:03:39 +0330

    define task priorities class and its colors

---
+++ app/src/main/java/com/androiddev/mytodoapp/data/models/Priority.kt
@@ -0,0 +1,14 @@
+package com.androiddev.mytodoapp.data.models
+
+import androidx.compose.ui.graphics.Color
+import com.androiddev.mytodoapp.ui.theme.HighPriorityColor
+import com.androiddev.mytodoapp.ui.theme.LowPriorityColor
+import com.androiddev.mytodoapp.ui.theme.MediumPriorityColor
+import com.androiddev.mytodoapp.ui.theme.NonePriorityColor
+
+enum class Priority(val color: Color) {
+    High(HighPriorityColor),
+    MEDIUM(MediumPriorityColor),
+    LOW(LowPriorityColor),
+    NONE(NonePriorityColor)
+}
