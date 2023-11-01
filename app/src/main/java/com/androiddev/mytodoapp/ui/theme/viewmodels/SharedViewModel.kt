e5cf328b417dd6dbe62fc7c2809935cbf990c622
shabestari <niloufar2012shabestari@gmail.com>
2023-11-01 14:39:07 +0330

    created viewmodel class to get data from repository

---
+++ app/src/main/java/com/androiddev/mytodoapp/ui/theme/viewmodels/SharedViewModel.kt
@@ -0,0 +1,27 @@
+package com.androiddev.mytodoapp.ui.theme.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.androiddev.mytodoapp.data.models.TodoTask
+import com.androiddev.mytodoapp.data.repositories.TodoRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SharedViewModel @Inject constructor(
+    private val repository: TodoRepository
+) : ViewModel() {
+
+    private val _allTasks = MutableStateFlow<List<TodoTask>>(emptyList())
+    val allTasks: StateFlow<List<TodoTask>> = _allTasks
+    fun getAllTasks() {
+        viewModelScope.launch {
+            repository.getAllTasks().collect {
+                _allTasks.value = it
+            }
+        }
+    }
+}
