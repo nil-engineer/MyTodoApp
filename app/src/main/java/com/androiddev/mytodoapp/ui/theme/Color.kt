d268f75cc5224af1ad25b14f3a574e1ad5cbe867
shabestari <niloufar2012shabestari@gmail.com>
2023-11-01 17:04:12 +0330

    added gray colors

--- app/src/main/java/com/androiddev/mytodoapp/ui/theme/Color.kt
+++ app/src/main/java/com/androiddev/mytodoapp/ui/theme/Color.kt
@@ -1,5 +1,7 @@
 package com.androiddev.mytodoapp.ui.theme
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,6 +11,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val LightGray = Color(0xFFFCFCFC)
+val MediumGray = Color(0xFF9C9C9C)
+val DarkGray = Color(0xFF141414)
 
 val LowPriorityColor = Color(0xFF00C980)
 val MediumPriorityColor = Color(0xFFFFC114)
